ab62fe19d76e36df3ee632add1beb932ed666f44
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-02-23 18:25:47 -0500

    only write non garbage

--- src/receiver.c
+++ src/receiver.c
@@ -94,12 +94,15 @@ void rrecv(unsigned short int myUDPport,
         exit(EXIT_FAILURE);
     }
 
-    int written;
-    written = fwrite(buffer, sizeof(char), sizeof(buffer), write_file);
-    if (written == 0) {
-    printf("Error during writing to file !");
-    }   
+    // Determine the actual size of the received data
+    size_t received_size = client_message;
 
+    // Write only the received data to the file
+    int written = fwrite(buffer, sizeof(char), received_size, write_file);
+    if (written < received_size) {
+        printf("Error during writing to file!");
+    }
+    
     // else 
     // check order (first 2 byes of the package)
     // if order doesnt match then send nack

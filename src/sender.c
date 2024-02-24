53ae8c520e3476c93690a01540f0b46be4827ae7
kim-dajeong <kimdajeong10@gmail.com>
2024-02-23 19:06:50 -0800

    Update sender.c

--- src/sender.c
+++ src/sender.c
@@ -96,7 +96,7 @@ void rsend(char* hostname,
 
         // Read byteNumber size of file
         fread(startRead, sizeof(char), byteNumber, read_file);
-        printf("String read: %s, packet number: %d\n", sender_message, index);
+        fprintf("String read: %s, packet number: %d\n", sender_message, index);
 
             // Send the message to server:
         sendto(socket_desc, sender_message, strlen(sender_message), 0, (struct sockaddr*)&server_addr, struct_length);

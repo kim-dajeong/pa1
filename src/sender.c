08efac3b0f8d131a8e0278f85571eb5f9b3a8304
kim-dajeong <kimdajeong10@gmail.com>
2024-02-23 17:04:39 -0800

    Update sender.c

--- src/sender.c
+++ src/sender.c
@@ -26,6 +26,7 @@ void rsend(char* hostname,
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(hostUDPport); //Check later
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
        // Open file for reading
     FILE* file = fopen(filename, "r");

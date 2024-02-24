18c1468d3b74f00f660542cf1462271439d81eed
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-02-23 19:05:37 -0500

    Update receiver.c

--- src/receiver.c
+++ src/receiver.c
@@ -90,7 +90,7 @@ void rrecv(unsigned short int myUDPport,
     // Receive client's message:
     size_t client_message = recvfrom(socket_desc, buffer, sizeof(buffer), 0, (struct sockaddr*)&address, &client_struct_length);  
     if (client_message < 0){
-        printf("Couldn't receive\n");
+    printf("Couldn't receive\n");
         exit(EXIT_FAILURE);
     }
 
@@ -99,7 +99,7 @@ void rrecv(unsigned short int myUDPport,
     size_t payload_size = client_message - udp_header_size;
 
     // Write only the payload data to the file
-    int written = fwrite(buffer + udp_header_size, sizeof(char), payload_size, write_file);
+    int written = fwrite(buffer, sizeof(char), payload_size, write_file);
     if (written < payload_size) {
         printf("Error during writing to file!");
     }

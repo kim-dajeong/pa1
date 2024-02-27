424275cf4b5a2988af900dbcd31941f50aead211
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-02-27 15:36:24 -0800

    Update sender.c

--- src/sender.c
+++ src/sender.c
@@ -45,6 +45,7 @@ Sender Notes
 */
 
 #define max_payload_size 1024 //! max data we can send over
+#define max_data_size 1018 //six bytes less than max payload to make space for the header 
 
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
@@ -88,15 +89,22 @@ void rsend(char* hostname,
     
     printf("Socket created successfully\n");
 
+    //sender_buffer total data with header
+    void *sender_buffer = malloc(max_payload_size);
+    memset(sender_buffer, 0, max_payload_size);
+    if (sender_buffer == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     //initallize void pointer for sender message to get raw bytes from the file
-    void *readfile_message = malloc(max_payload_size);
-    memset(readfile_message, 0, max_payload_size);
+    void *readfile_data = malloc(max_data_size);
+    memset(readfile_data, 0, max_data_size);
 
     //receive buffer setup
-    void *buffer= malloc(max_payload_size);
-    memset(buffer, 0, max_payload_size);
-    
+    void *ack_buffer= malloc(max_payload_size);
+    memset(ack_buffer, 0, max_payload_size);
+
     //initallize the sending while loop
     int bytesRead = 0;
     int index = 0;
@@ -107,31 +115,38 @@ void rsend(char* hostname,
         byteNumber = (max_payload_size < (bytesToTransfer - bytesRead)) ? max_payload_size : (bytesToTransfer - bytesRead);
 
         //initallize void pointer for sender message to get raw bytes from the file
-        void *readfile_message = malloc(byteNumber);
-        memset(readfile_message, 0, byteNumber);
+        memset(readfile_data, 0, byteNumber);
+        memset(sender_buffer, 0, byteNumber);
 
         // Read byteNumber size of file
         fseek(read_file, bytesRead, SEEK_SET);
-        fread(readfile_message, 1, byteNumber, read_file);
+        fread(readfile_data, 1, byteNumber, read_file);
+
+        // Copy the two uint8_t values to the start of the new buffer
+        uint8_t *ptr = (uint8_t *)sender_buffer;
+        ptr[0] = 0;
+        ptr[1] = 0;
+        memcpy(ptr + 2, readfile_data, byteNumber);
+
 
         // Send the message to server:
-        if(sendto(socket_desc, readfile_message, strlen(readfile_message), 0, (struct sockaddr*)&server_addr, struct_length)<0){
+        if(sendto(socket_desc, sender_buffer, strlen(sender_buffer), 0, (struct sockaddr*)&server_addr, struct_length)<0){
             printf("Unable to send message\n");
             exit(EXIT_FAILURE);
         }
 
-        size_t client_message = recvfrom(socket_desc, buffer, max_payload_size, 0, (struct sockaddr*)&server_addr, &struct_length);  
+        size_t client_message = recvfrom(socket_desc, ack_buffer, max_payload_size, 0, (struct sockaddr*)&server_addr, &struct_length);  
         if (client_message < 0){
             printf("Couldn't receive\n");
         exit(EXIT_FAILURE);
         }
 
         // testing an ack backbone 
-        if(*(int*)buffer == 22){ 
+        if(*(int*)ack_buffer == 22){ 
             printf("Hello I Hear You!");
         }
         
-        printf("%ld\n",strlen(readfile_message));
+        printf("%ld\n",strlen(sender_buffer));
 
         index++;
         bytesRead += byteNumber;

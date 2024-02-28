b68dec1dcca77eb207f420327cdefca93a3ef0b1
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-02-27 18:59:52 -0800

    Update sender.c

--- src/sender.c
+++ src/sender.c
@@ -143,11 +143,11 @@ void rsend(char* hostname,
         }
        
         
-        size_t client_message = recvfrom(socket_desc, ack_buffer, max_payload_size, 0, (struct sockaddr*)&server_addr, &struct_length);  
+        /*size_t client_message = recvfrom(socket_desc, ack_buffer, max_payload_size, 0, (struct sockaddr*)&server_addr, &struct_length);  
         if (client_message < 0){
             printf("Couldn't receive\n");
             exit(EXIT_FAILURE);
-        }
+        }*/
         
 
         uint8_t ack_message;
